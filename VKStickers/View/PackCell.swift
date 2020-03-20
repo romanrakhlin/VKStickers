8b5b8b3d6a96cc1fbbb3f5b497f767494d5863f0
Roman <rrakhlin@gmail.com>
2020-03-21 01:45:17 +0300

    initial commit

---
+++ VKStickers/View/PackCell.swift
@@ -0,0 +1,24 @@
+//
+//  PackCell.swift
+//  VKStickers
+//
+//  Created by Roman Rakhlin on 18.03.2020.
+//  Copyright © 2020 Roman Rakhlin. All rights reserved.
+//
+
+import UIKit
+
+class PackCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
